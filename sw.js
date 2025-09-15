572cc2e7d8ac07e0ed9fdb699992a68e13cda797
Alex Mwaura <alecsmwaura@gmail.com>
2025-09-16 02:39:20 +0300

    test

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('static').then(cache => {
-      const assets = ['/', '/index.html', '/css/styles.css', '/js/script.js', 
-                      'icons/appIcon.png', 'icons/standAloneIcon.png','icons/logo.png'];
+      const assets = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/script.js',
+  '/js/functional.js',
+  '/manifest.json',
+  '/icons/icon.png',
+  '/icons/sicon.png',
+  '/icons/logo.png',
+  '/icons/favicon.ico',
+];
       return Promise.all(
         assets.map(url => 
           cache.add(url).catch(err => console.warn('Failed to cache', url, err))
