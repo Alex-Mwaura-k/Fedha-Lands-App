039055e5eb1da023f96eef79acc3b757f008c6cb
Alex <alecsmwaura@gmail.com>
2025-12-09 23:25:46 +0300

    update

--- src/data/faqData.js
+++ src/data/faqData.js
@@ -3,30 +3,37 @@ export const faqData = [
     id: 1,
     question: "How long does it take to get my Title Deed?",
     answer:
-      "For cash buyers, the transfer process takes 30-45 days. For installment buyers, the process begins immediately after you complete your final payment.",
+      "For cash buyers, the transfer process takes from between 14-60 days. For installment buyers, the process begins immediately after you complete your final payment and not exceeding 90 days.",
   },
   {
     id: 2,
     question: "Do you offer free site visits?",
     answer:
-      "Yes! We offer free transport to our projects every Wednesday and Saturday. Pickup is at our Ruiru office (Nyongo House) at 8:00 AM.",
+      "Yes! We offer free transport to our projects every Wednesday and Saturday. Pickup is at our Ruiru office (Nyongo Plaza) at 8:00 AM.",
   },
   {
     id: 3,
     question: "Can I pay for the land in installments?",
     answer:
-      "Absolutely. We have flexible payment plans of up to 6 months. You can book a plot with a deposit (e.g., Ksh 50,000) and pay the balance monthly.",
+      "Absolutely. We have flexible payment plans of between 6-12 months. You can book a plot with a deposit (e.g., Ksh 50,000) and pay the balance monthly.",
   },
   {
     id: 4,
     question: "Are the title deeds freehold or leasehold?",
     answer:
-      "All our agricultural and residential plots (Kithyoko, Malindi, Makutano) come with Freehold Title Deeds, meaning you own the land indefinitely.",
+      "All our commercial, agricultural and residential plots (Kithyoko, Malindi, Makutano) come with Freehold Title Deeds, meaning you own the land indefinitely.",
   },
   {
     id: 5,
     question: "What happens if I cannot complete payment?",
     answer:
       "We understand that financial situations change. Contact us immediately to restructure your payment plan. In worst-case scenarios, we can offer a refund as per the terms signed in your Sale Agreement.",
+  },
+
+  {
+    id: 6,
+    question: "How can i reach you?",
+    answer:
+      "We available across all online platforms, Facebook, Instagram, Tiktok, WhatsApp and on calls via 0715 113 103.",
   },
 ];
