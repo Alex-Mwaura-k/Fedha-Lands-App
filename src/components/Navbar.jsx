154efbdd78170f7c0b140116e3630d509f30560e
Alex <alecsmwaura@gmail.com>
2025-12-12 09:14:09 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -231,11 +231,6 @@ const Navbar = () => {
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/careers">
-                  Careers
-                </Link>
-              </li>
-              <li className="nav-item">
                 <Link className="nav-link" to="/contact">
                   Contact
                 </Link>
