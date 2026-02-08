97707c2a5f083b6c9143250b01702d9621ee93cf
Alex <alecsmwaura@gmail.com>
2026-02-08 23:53:11 +0300

    Fix: Set executable permissions for build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -10,3 +10,21 @@ python manage.py collectstatic --no-input
 
 # 3. Apply Database Migrations
 python manage.py migrate
+
+python manage.py shell << END
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+
+if username and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print("✅ Superuser created successfully")
+    else:
+        print("ℹ️ Superuser already exists")
+else:
+    print("⚠️ Skipping superuser creation: Environment variables not set")
+END
